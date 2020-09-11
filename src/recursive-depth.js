18799dba3af7fde0419f735fe70be09e1b43dcc9
Andrey <AAKlimkov@gmail.com>
2020-09-11 17:54:17 +0300

    feat: add solution transform-array

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    var count=1
+    for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i]== 'object') {
+      count++
+    }
+      
+    }
+return count
   }
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,5 +1,62 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
-return arr
+    var j =0
+    var result = []
+ 
+
+    // arr[arr.length]== '--discard-next' ||'--double-next' ? arr.pop() : arr
+    // arr[0] == "--discard-prev" || '--double-prev' ? i=1 : i = 0;
+
+    for (let i=0; i < arr.length; i++) {
+        
+        switch (arr[i]) {
+            case "--discard-prev":
+                if (i==0) {
+                    break
+                }
+                    result.pop()
+                    j--
+                    break;
+                
+            case "--discard-next":
+
+                if (i==arr.length-1) {
+                    break
+                }
+                if(arr[i+2]=='--double-prev'){
+                    i=i+2
+                    break
+                } else if(arr[i+2]=='--discard-prev'){
+                  i=i+2
+                  break
+                } else {
+                    i++
+                    break;
+                }
+              
+            case '--double-prev':
+                if (i==0) {
+                    break
+                }
+                result[j]=result[j-1]
+                j++
+                break;
+            case '--double-next':
+                if (i==arr.length-1) {
+                    break
+                }
+                    result[j]=arr[i+1]
+                    j++
+                    break;  
+                
+                
+            default:
+                result[j]=arr[i]
+                j++
+                break;
+        }
+    
+    }
+return result
 };
