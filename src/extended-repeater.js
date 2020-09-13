f8cb987c8ee83c30b963a9caea31645fb41a8ca5
Andrey <AAKlimkov@gmail.com>
2020-09-13 13:30:12 +0300

    feat: add solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,23 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  
+  const count = options.repeatTimes ? options.repeatTimes : 1
+  const separator = options.separator ? options.separator : '+'
+  const removeCharacterInTheend = separator.length
+  const addition = options.addition? options.addition : ""
+  const additionSeparator = options.additionSeparator ? options.additionSeparator : ""
+  const additionAndSeparator = addition.concat(additionSeparator)
+  const additionRepeatTimes = options.additionRepeatTimes ? options.additionRepeatTimes : 1
+  const additionAndSeparatorRepeat = additionAndSeparator.repeat(additionRepeatTimes)
+  const finalAddition = additionAndSeparatorRepeat ? additionAndSeparatorRepeat.slice(0,-additionSeparator.length) : addition
+  const finalAddition2 = finalAddition ? finalAddition: addition
+  const strAndAddition = str.concat(finalAddition2)
+  const strAndAdditionAndSeparanor = strAndAddition.concat(separator)
+  const fullSTR = strAndAdditionAndSeparanor.repeat(count)
+  return fullSTR.slice(0,-removeCharacterInTheend)
+// return  addition
+
+
 };
   
