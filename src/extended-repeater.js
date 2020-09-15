8b74526b0af4ee9f913c86c18a22abdb434d81a3
Andrey <AAKlimkov@gmail.com>
2020-09-15 20:52:28 +0300

    feat: add solution for Null and false

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,18 +1,27 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function repeater(str, options) {
-  
+  const str2 = str? str.toString() : "null"
   const count = options.repeatTimes ? options.repeatTimes : 1
-  const separator = options.separator ? options.separator : '+'
+  const separator = options.separator ? options.separator.toString() : '+'
   const removeCharacterInTheend = separator.length
-  const addition = options.addition? options.addition : ""
-  const additionSeparator = options.additionSeparator ? options.additionSeparator : ""
+  let addition
+  if (options.addition===false) {
+    addition ="false"
+  } else if(options.addition === null) {
+    addition = "null"
+  } else{
+    addition = options.addition? options.addition.toString() : ""
+  }
+   
+
+  const additionSeparator = options.additionSeparator ? options.additionSeparator.toString() : ""
   const additionAndSeparator = addition.concat(additionSeparator)
   const additionRepeatTimes = options.additionRepeatTimes ? options.additionRepeatTimes : 1
   const additionAndSeparatorRepeat = additionAndSeparator.repeat(additionRepeatTimes)
   const finalAddition = additionAndSeparatorRepeat ? additionAndSeparatorRepeat.slice(0,-additionSeparator.length) : addition
   const finalAddition2 = finalAddition ? finalAddition: addition
-  const strAndAddition = str.concat(finalAddition2)
+  const strAndAddition = str2.concat(finalAddition2)
   const strAndAdditionAndSeparanor = strAndAddition.concat(separator)
   const fullSTR = strAndAdditionAndSeparanor.repeat(count)
   return fullSTR.slice(0,-removeCharacterInTheend)
